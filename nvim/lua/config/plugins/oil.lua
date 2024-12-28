bc281b63233ee2f3c2109e8de77fbe8b03f222be
iferdel <ignaferdel@gmail.com>
2024-12-28 18:58:07 -0500

    reorder oil keymaps into oil.lua

--- nvim/lua/config/plugins/oil.lua
+++ nvim/lua/config/plugins/oil.lua
@@ -4,14 +4,25 @@ return {
     enabled = true,
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {
-
-      view_options = {
-        show_hidden = true,
-      }
-    },
-    -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+
+    config = function()
+      require("oil").setup {
+        view_options = {
+          show_hidden = true,
+        },
+
+        keymaps = {
+          ["<C-h>"] = false, -- deactivate C-h for splitting window with selected file in oil
+          ["<M-h>"] = "actions.select_split",
+        },
+        -- Open parent directory in current window
+        vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = 'Open parent directory' }),
+        -- Open parent directory in floating window
+        vim.keymap.set("n", "<space>-", require("oil").toggle_float),
+      }
+    end,
+
   },
 }
