48dc7d7c246c2658c02b87d5db042b920a9d6c6e
iferdel <ignaferdel@gmail.com>
2024-12-29 11:43:40 -0500

    fix: open respective markdown in obsidian

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,10 @@
 vim.keymap.set("n", "<leader>co", function()
-  -- vim.cmd("!xdg-open 'obsidian://open?vault=iferdel&file=%'")
-  --vim.cmd("!open -e 'obsidian://open?vault=iferdel&file=%'")
-  vim.ui.open('obsidian://open?vault=iferdel&file=%%')
+  local file_path = vim.fn.expand("%:p") -- Get the full file path of the current buffer
+  if file_path and file_path ~= "" then
+    local obsidian_url = "obsidian://open?vault=iferdel&file=" ..
+        vim.fn.fnamemodify(file_path, ":t") -- Use the file name only
+    vim.ui.open(obsidian_url)
+  else
+    vim.notify("No file is currently open or recognized!", vim.log.levels.ERROR)
+  end
 end, { desc = "Open in Obsidian", noremap = true, silent = true })
