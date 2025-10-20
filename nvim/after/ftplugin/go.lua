e040e0e9dcf90181d548eba3113812f1a1c4d374
iferdel <ignaferdel@gmail.com>
2025-10-20 12:14:20 -0300

    go specific keymaps to access different sources

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -6,3 +6,27 @@ set.shiftwidth = 2
 set.tabstop = 2
 set.number = true
 set.relativenumber = true
+
+-- Highlight word under cursor after 5 seconds
+set.updatetime = 5000
+
+-- Create autocmd group for this buffer
+local group = vim.api.nvim_create_augroup('GoDocumentHighlight', { clear = false })
+
+-- Highlight references when cursor stops moving
+vim.api.nvim_create_autocmd('CursorHold', {
+  group = group,
+  buffer = 0,
+  callback = function()
+    vim.lsp.buf.document_highlight()
+  end,
+})
+
+-- Clear highlights when cursor moves
+vim.api.nvim_create_autocmd('CursorMoved', {
+  group = group,
+  buffer = 0,
+  callback = function()
+    vim.lsp.buf.clear_references()
+  end,
+})
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
+require("config.lang.go")
 
 vim.opt.shiftwidth = 4            -- indentation
 vim.opt.clipboard = "unnamedplus" -- paste whatever is in the clipboard buffer globally
@@ -50,74 +51,3 @@ vim.keymap.set("n", "<space>st", function() -- opens a new terminal at the botto
 
   job_id = vim.b.terminal_job_id
 end, { desc = "Open bottom terminal" })
-
--- Go utilities menu with which-key detection
-local has_which_key = pcall(require, 'which-key')
-
-if has_which_key then
-  -- Use sub-keymaps that which-key will show as a menu
-  vim.keymap.set("n", "<space>gos", function()
-    local go_root = vim.fn.system("go env GOROOT"):gsub("\n", "")
-    local opts = require('telescope.themes').get_ivy({
-      cwd = go_root .. "/src",
-      find_command = { "find", ".", "-name", "*.go", "-type", "f" },
-      prompt_title = "Go Source Code",
-    })
-    require('telescope.builtin').find_files(opts)
-  end, { desc = "Source code (stdlib)" })
-
-  vim.keymap.set("n", "<space>gom", function()
-    local go_path = vim.fn.system("go env GOPATH"):gsub("\n", "")
-    local opts = require('telescope.themes').get_ivy({
-      cwd = go_path .. "/pkg/mod",
-      find_command = { "find", ".", "-name", "*.go", "-type", "f" },
-      prompt_title = "Go Modules",
-    })
-    require('telescope.builtin').find_files(opts)
-  end, { desc = "Modules (GOPATH)" })
-
-  vim.keymap.set('n', '<space>goc', function()
-    vim.fn.jobstart({ "xdg-open", "https://go-cookbook.com/snippets" })
-  end, { desc = "Cookbook (browser)" })
-else
-  -- Fallback to vim.ui.select when which-key is not available
-  vim.keymap.set("n", "<space>go", function()
-    local go_actions = {
-      { name = "Go Source Code (stdlib)",  action = "source" },
-      { name = "Go Modules (GOPATH)",      action = "modules" },
-      { name = "Go Cookbook (browser)",    action = "cookbook" },
-    }
-
-    vim.ui.select(go_actions, {
-      prompt = "Go Utilities:",
-      format_item = function(item)
-        return item.name
-      end,
-    }, function(choice)
-      if not choice then return end
-
-      if choice.action == "source" then
-        -- Go source code navigation
-        local go_root = vim.fn.system("go env GOROOT"):gsub("\n", "")
-        local opts = require('telescope.themes').get_ivy({
-          cwd = go_root .. "/src",
-          find_command = { "find", ".", "-name", "*.go", "-type", "f" },
-          prompt_title = "Go Source Code",
-        })
-        require('telescope.builtin').find_files(opts)
-      elseif choice.action == "modules" then
-        -- Go modules navigation
-        local go_path = vim.fn.system("go env GOPATH"):gsub("\n", "")
-        local opts = require('telescope.themes').get_ivy({
-          cwd = go_path .. "/pkg/mod",
-          find_command = { "find", ".", "-name", "*.go", "-type", "f" },
-          prompt_title = "Go Modules",
-        })
-        require('telescope.builtin').find_files(opts)
-      elseif choice.action == "cookbook" then
-        -- Go Cookbook
-        vim.fn.jobstart({ "xdg-open", "https://go-cookbook.com/snippets" })
-      end
-    end)
-  end, { desc = "Go utilities" })
-end
