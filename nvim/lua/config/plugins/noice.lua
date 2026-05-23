2af996897aa67b09683de5fb6eba96b38cee56c1
iferdel <ignaferdel@gmail.com>
2026-05-22 22:52:00 -0500

    improvement of noice plugin

--- nvim/lua/config/plugins/noice.lua
+++ nvim/lua/config/plugins/noice.lua
@@ -10,58 +10,54 @@ return {
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
         },
+        signature = { enabled = false }, -- defer to blink.cmp's signature window
+        hover = { enabled = true },
+        progress = {
+          enabled = true,
+          view = "mini",                 -- gopls / pylsp indexing in bottom-right mini view
+        },
+      },
+      messages = {
+        enabled = true,
+        view = "mini",                   -- routine messages: calm bottom-right strip
+        view_error = "notify",           -- errors still pop as notifications
+        view_warn = "notify",            -- warnings too
+        view_history = "messages",
+        view_search = "virtualtext",     -- search count next to cursor, not cmdline
       },
       routes = {
+        -- One collapsed route for routine line/yank/write/E162 noise
         {
           filter = {
             event = "msg_show",
-            kind = "emsg",
-            find = "E162",
+            any = {
+              { find = "%d+ lines yanked" },
+              { find = "%d+ more lines" },
+              { find = "%d+ fewer lines" },
+              { find = "%d+ lines indented" },
+              { find = "written" },
+              { kind = "emsg", find = "E162" },
+            },
           },
           opts = { skip = true },
         },
-        -- Skip yank messages
-        {
-          filter = {
-            event = "msg_show",
-            find = "%d+ lines yanked",
-          },
-          opts = { skip = true },
+      },
+      views = {
+        cmdline_popup = {
+          position = { row = "50%", col = "50%" }, -- true vertical+horizontal center
+          size = { width = 60, height = "auto" },
+          border = { style = "rounded" },
         },
-        -- Skip "more lines" and "fewer lines" messages
-        {
-          filter = {
-            event = "msg_show",
-            find = "%d+ more lines",
-          },
-          opts = { skip = true },
+        popupmenu = {
+          border = { style = "rounded" },
         },
-        {
-          filter = {
-            event = "msg_show",
-            find = "%d+ fewer lines",
-          },
-          opts = { skip = true },
-        },
-        -- Skip "lines indented" messages
-        {
-          filter = {
-            event = "msg_show",
-            find = "%d+ lines indented",
-          },
-          opts = { skip = true },
-        },
-        -- Skip written messages
-        {
-          filter = {
-            event = "msg_show",
-            find = "written",
-          },
-          opts = { skip = true },
+        mini = {
+          win_options = { winblend = 0 }, -- fully opaque mini view, readable text
         },
       },
       presets = {
         bottom_search = true,
+        command_palette = true,           -- combined cmdline + popupmenu float
         long_message_to_split = true,
         inc_rename = false,
         lsp_doc_border = true,
