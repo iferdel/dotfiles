e8e6bd722dacf09845bedd715e9b7f496af2f01c
iferdel <ignaferdel@gmail.com>
2026-05-22 22:25:23 -0500

    feat(nvim): add mini.indentscope plugin
    - Configure indent guides with static symbol and no animation
    - Disable in special buffers (oil, dbee, lazy, telescope, etc.) and terminals
    - Update lazy-lock.json (drop mini.statusline and nvim-web-devicons)

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -22,4 +22,38 @@ return {
       })
     end,
   },
+  {
+    'echasnovski/mini.indentscope',
+    version = false,
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local indentscope = require("mini.indentscope")
+      indentscope.setup({
+        symbol = "│",
+        draw = {
+          animation = indentscope.gen_animation.none(), -- no animation: calmer look
+        },
+        options = {
+          try_as_border = true,
+        },
+      })
+
+      -- Disable in special buffers where an indent line is just noise
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help", "oil", "dbee", "lazy", "mason",
+          "TelescopePrompt", "noice", "notify",
+          "checkhealth", "qf",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("TermOpen", {
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  },
 }
